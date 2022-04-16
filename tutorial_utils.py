a16b8b5bc816847bca99a2acbf4d2fdfb81ccf82
cebirnie92 <cebirnie@googlemail.com>
2022-04-16 17:13:18 +0300

    Updating Tutorial one and including data

---
+++ tutorial_utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+import itertools
+
+
+def regular_patching_2D(data, patchsize=[64, 64], step=[16, 16], verbose=True):
+    """ Regular sample and extract patches from a 2D array
+    :param data: np.array [y,x]
+    :param patchsize: tuple [y,x]
+    :param step: tuple [y,x]
+    :param verbose: boolean
+    :return: np.array [patch#, y, x]
+    """
+
+    # find starting indices
+    x_start_indices = np.arange(0, data.shape[0] - patchsize[0], step=step[0])
+    y_start_indices = np.arange(0, data.shape[1] - patchsize[1], step=step[1])
+    starting_indices = list(itertools.product(x_start_indices, y_start_indices))
+
+    if verbose:
+        print('Extracting %i patches' % len(starting_indices))
+
+    patches = np.zeros([len(starting_indices), patchsize[0], patchsize[1]])
+
+    for i, pi in enumerate(starting_indices):
+        patches[i] = data[pi[0]:pi[0]+patchsize[0], pi[1]:pi[1]+patchsize[1]]
+
+    return patches
+
+def add_whitegaussian_noise(d, sc=0.5):
+    n = np.random.normal(size=d.shape)
+
+    return d + (n * sc), n
